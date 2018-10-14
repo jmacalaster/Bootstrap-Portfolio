871a16a66ab65f5776e03826651f1d50589e8183
JMac <julia@defmethod.com>
2018-10-14 11:47:51 -0400

    Updated Portfolio for MVC folder structure

---
+++ controllers/todo_controller.js
@@ -0,0 +1,76 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model to use its database functions.
+var todos = require("../models/todo.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  todos.all(function(data) {
+    var hbsObject = {
+      todo: data
+    };
+    console.log("hbsObject: " + hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.get("/portfolio", function(req, res) {
+    todos.all(function(data) {
+      var hbsObject = {
+        todo: data
+      };
+      console.log("hbsObject: " + hbsObject);
+      res.render("portfolio", hbsObject);
+    });
+});
+
+router.get("/contact", function(req, res) {
+    todos.all(function(data) {
+      var hbsObject = {
+        todo: data
+      };
+      console.log("hbsObject: " + hbsObject);
+      res.render("contact", hbsObject);
+    });
+});
+
+router.post("/api/todos", function(req, res) {
+  todos.create([
+    "task", "completed"
+  ], [
+    req.body.task, req.body.completed
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/todos/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  todos.update({
+    completed: req.body.completed
+  }, condition, function(result) {
+      res.status(200).end();
+  });
+});
+
+router.delete("/api/todos/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  todos.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
